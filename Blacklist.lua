19a15b5b690f3bf77e597c831907073f8e2dc2d0
NoUGotbannedlol <98497646+NoUGotbannedlol@users.noreply.github.com>
2022-02-20 12:02:19 -0500

    Update Blacklist.lua

--- Blacklist.lua
+++ Blacklist.lua
@@ -1,5 +1,7 @@
 -- I Dont Care if you see this its just a blacklist for the people i dont like (they will get auto kicked and ip logged)
-getgenv().blacklist = {
-"Test",
- 123,
+Blacklist = {
+    [2623517878] = true, --test
+    ["test"] =  true,
 }
+
+return ModIDS
