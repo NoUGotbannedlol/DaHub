ca17c1a287c1e61415d037b2a0990ae18290b2bc
NoUGotbannedlol <98497646+NoUGotbannedlol@users.noreply.github.com>
2022-02-20 12:31:05 -0500

    Update Blacklist.lua

--- Blacklist.lua
+++ Blacklist.lua
@@ -1,7 +1,7 @@
 -- I Dont Care if you see this its just a blacklist for the people i dont like (they will get auto kicked and ip logged)
 Blacklist = {
-    [1981211466] = true, --test
-    ["test"] =  true,
+    [2207291] = true, --test
+    [3227974128] =  true, -- bitch boy
 }
 
 return Blacklist
